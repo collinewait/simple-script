33ad1c4c02807e0560540108a32b15cae0d9d717
collinewait <collinewait17@gmail.com>
2020-03-20 01:21:09 +0300

    display a list of scripts

--- simple-script-ui/src/app/scripts/scripts.component.ts
+++ simple-script-ui/src/app/scripts/scripts.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ScriptsService } from './scripts.service';
+
 @Component({
   selector: 'app-scripts',
   templateUrl: './scripts.component.html',
   styleUrls: ['./scripts.component.scss']
 })
 export class ScriptsComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['id', 'script', 'runResults'];
+  data = [];
+  isLoadingResults = false;
+  constructor(private scpt: ScriptsService) { }
 
   ngOnInit(): void {
+    this.scpt.getScripts()
+    .subscribe((res: any) => {
+      this.data = Object.values(res.data);
+      this.isLoadingResults = false;
+    }, err => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
   }
 
 }
---
+++ simple-script-ui/src/app/scripts/scripts.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = 'http://localhost:3000/api/v1/scripts';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScriptsService {
+
+  constructor(private http: HttpClient) { }
+
+  getScripts() {
+    return this.http.get<any>(apiUrl);
+  }
+}
