451209648c1e33aed3b690783697b88f5e90d1cd
collinewait <collinewait17@gmail.com>
2020-04-11 20:53:38 +0300

    return a custom message on duplicates

--- packages/simple-script-server/src/util/asyncHandler.js
+++ packages/simple-script-server/src/util/asyncHandler.js
@@ -3,7 +3,15 @@ const asyncHandler = callback => async (req, res, next) => {
     await callback(req, res, next);
   } catch (error) {
     const errStatus = error.kind === 'ObjectId' ? 404 : error.status || 500;
-    res.status(errStatus).send({
+
+    if (error.name === 'MongoError' && error.code === 11000) {
+      return res.status(400).send({
+        status: 400,
+        error: 'Duplicates are not allowed',
+      });
+    }
+
+    return res.status(errStatus).send({
       status: errStatus,
       error: error.message,
     });
