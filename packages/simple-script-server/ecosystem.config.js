c6457bb8a8d0f71755d57e73df55ddae20d3802f
collinewait <collinewait17@gmail.com>
2020-04-16 23:17:34 +0300

    run the app with pm2

---
+++ packages/simple-script-server/ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'simple-script-server',
+      script: 'dist/index.js',
+
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      args: 'one two',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
