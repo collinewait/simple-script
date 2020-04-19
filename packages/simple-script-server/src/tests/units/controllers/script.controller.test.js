e4baa6673cacc2898327b6113343ce56bbe6160d
collinewait <collinewait17@gmail.com>
2020-04-19 13:35:11 +0300

    test updateScript in script controllers

--- packages/simple-script-server/src/tests/units/controllers/script.controller.test.js
+++ packages/simple-script-server/src/tests/units/controllers/script.controller.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import sinon from 'sinon';
 import chai from 'chai';
 import dirtyChai from 'dirty-chai';
@@ -7,6 +8,7 @@ import {
   createScript,
   getAllScripts,
   getSingleScript,
+  updateScript,
 } from '../../../controllers/script.controller';
 
 const { expect } = chai;
@@ -15,6 +17,7 @@ chai.use(chaiAsPromised);
 chai.use(dirtyChai);
 
 describe('Script controllers', () => {
+  const invalidOpsMsg = 'request contains invalid operations';
   context('createScript', () => {
     const mockRequest = () => ({
       body: {
@@ -33,7 +36,6 @@ describe('Script controllers', () => {
       return res;
     };
 
-    const invalidOpsMsg = 'request contains invalid operations';
     const script = { script: 'script-here', _id: 'some-id-here', runResults: 'some-script-output' };
 
     it('should return request missing operations message when no operations sent', async () => {
@@ -225,6 +227,169 @@ describe('Script controllers', () => {
       await getSingleScript(mockReq, mockRes);
 
       expect(mockRes.status.calledWith(200)).to.be.true();
+    });
+  });
+
+  context('updateScript', () => {
+    const mockRequest = () => ({
+      body: {
+        script: '',
+      },
+    });
+    const mockResponse = () => {
+      const res = {};
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+      return res;
+    };
+
+    it('should throw an error when the script is empty', async () => {
+      const mockReq = mockRequest();
+      const mockRes = mockResponse();
+
+      await expect(updateScript(mockReq, mockRes)).to.be.rejectedWith(invalidOpsMsg);
+    });
+
+    it('should throw an error when the script is not defined', async () => {
+      const mockReq = {
+        ...mockRequest(),
+        body: {
+          ...mockRequest().body,
+          script: null,
+        },
+      };
+      const mockRes = mockResponse();
+
+      await expect(updateScript(mockReq, mockRes)).to.be.rejectedWith(invalidOpsMsg);
+    });
+
+    it('should throw an error when the script is defined but invalid', async () => {
+      const mockReq = {
+        ...mockRequest(),
+        body: {
+          ...mockRequest().body,
+          script: 'some-invalid-script',
+        },
+      };
+      const mockRes = mockResponse();
+
+      await expect(updateScript(mockReq, mockRes)).to.be.rejectedWith(invalidOpsMsg);
+    });
+
+    it('should return an updated script with a 200 status code', async () => {
+      const updatingScript = 'DoThisThing(string)';
+      const scriptToUpdate = {
+        _id: 'some-id',
+        runResults: [],
+        script: 'DoThisThing(string)\nDoThisThing(string)',
+      };
+      const mockReq = {
+        ...mockRequest(),
+        body: {
+          ...mockRequest().body,
+          script: updatingScript,
+        },
+        context: {
+          script: {
+            save: sinon.stub().returns({ ...scriptToUpdate, script: updatingScript }),
+          },
+        },
+      };
+      const mockRes = mockResponse();
+
+      await updateScript(mockReq, mockRes);
+
+      expect(mockRes.status.calledWith(200)).to.be.true();
+      expect(
+        mockRes.json.calledWith({
+          message: 'success',
+          status: 200,
+          data: {
+            id: scriptToUpdate._id,
+            runResults: scriptToUpdate.runResults,
+            script: updatingScript,
+          },
+        }),
+      ).to.be.true();
+    });
+
+
+    it('should not update runResults when the scripts are the same', async () => {
+      const updatingScript = 'DoThisThing(string)';
+      const scriptToUpdate = {
+        _id: 'some-id',
+        runResults: [19],
+        script: 'DoThisThing(string)',
+      };
+      const mockReq = {
+        ...mockRequest(),
+        body: {
+          ...mockRequest().body,
+          script: updatingScript,
+        },
+        context: {
+          script: {
+            save: sinon.stub().returns({ ...scriptToUpdate, script: updatingScript }),
+          },
+        },
+      };
+      const mockRes = mockResponse();
+
+      await updateScript(mockReq, mockRes);
+
+      expect(mockRes.status.calledWith(200)).to.be.true();
+      expect(
+        mockRes.json.calledWith({
+          message: 'success',
+          status: 200,
+          data: {
+            id: scriptToUpdate._id,
+            runResults: scriptToUpdate.runResults,
+            script: updatingScript,
+          },
+        }),
+      ).to.be.true();
+    });
+
+    it('should update runResults when the scripts are different', async () => {
+      const updatingScript = 'DoThisThing(string)\nDoThisThing(string)';
+      const scriptToUpdate = {
+        _id: 'some-id',
+        runResults: [19],
+        script: 'DoThisThing(string)',
+      };
+      const mockReq = {
+        ...mockRequest(),
+        body: {
+          ...mockRequest().body,
+          script: updatingScript,
+        },
+        context: {
+          script: {
+            save: sinon.stub().returns({
+              ...scriptToUpdate,
+              script: updatingScript,
+              runResults: [],
+            }),
+          },
+        },
+      };
+      const mockRes = mockResponse();
+
+      await updateScript(mockReq, mockRes);
+
+      expect(mockRes.status.calledWith(200)).to.be.true();
+      expect(
+        mockRes.json.calledWith({
+          message: 'success',
+          status: 200,
+          data: {
+            id: scriptToUpdate._id,
+            runResults: [],
+            script: updatingScript,
+          },
+        }),
+      ).to.be.true();
     });
   });
 });
